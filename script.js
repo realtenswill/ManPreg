58569c2581baea524c78e5e3b5987fec337a51be
realtenswill <44103001+realtenswill@users.noreply.github.com>
2025-07-24 23:12:28 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -13,14 +13,28 @@ const name = [
     "Pregnant Amish",
     "Man Preg CEO",
     "Brit Preg",
-    "Preg Scream",
+    "Screaming Prego",
     "Pregnant Donny Trump",
     "Pregnant Willy G"
 ];
 
-const randomIndex = Math.floor(Math.random() * image.length);
-const randomImage = image[5];
-const randomName = name[5];
+  function getDayOfYear() {
+    const now = new Date();
+    const start = new Date(now.getFullYear(), 0, 0);
+    const diff = now - start;
+    const oneDay = 1000 * 60 * 60 * 24;
+    return Math.floor(diff / oneDay);
+  }
+
+//const randomIndex = Math.floor(Math.random() * image.length);
+//const randomImage = image[5];
+//const randomName = name[5];
+
+const dayOfYear = getDayOfYear();
+const index = dayOfYear % image.length;
+
+const randomImage = image[index];
+const randomName = name[index];
 
 const imageElement = document.getElementById("RandomPreg");
 imageElement.src = randomImage;
